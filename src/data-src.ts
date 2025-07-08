6746f834d1c4a4f6be939703ed59afe81aec3ab2
Ranimmmmm <rmouadb66@gmail.com>
2025-07-08 13:58:19 +0100

    update db url

--- src/data-src.ts
+++ src/data-src.ts
@@ -10,7 +10,7 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
- /*  host: process.env.DB_HOST,
+ /* host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
