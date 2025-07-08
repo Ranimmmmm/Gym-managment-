cd915f6161e0d8c6daa3f1ffa64dbe70c7cc968c
Ranimmmmm <rmouadb66@gmail.com>
2025-07-08 13:21:58 +0100

    correct db_url

--- src/data-src.ts
+++ src/data-src.ts
@@ -10,11 +10,11 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
-  /* host: process.env.DB_HOST,
+ /*  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE, */
+  database: process.env.DB_DATABASE,  */
   synchronize: true,
   logging: false,
   //dropSchema: true,
