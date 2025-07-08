2552962bd66b3ce4fc5ad745789169f68892f96f
Ranimmmmm <rmouadb66@gmail.com>
2025-07-08 14:08:23 +0100

    update connection string

--- src/data-src.ts
+++ src/data-src.ts
@@ -10,6 +10,7 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },  
  /* host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USERNAME,
